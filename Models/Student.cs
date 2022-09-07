dfc70d6be95c98ef57da9d3697eede28f36c2363
Your Name <you@example.com>
2022-09-07 16:23:06 +0700

    0709

---
+++ Models/Student.cs
@@ -0,0 +1,23 @@
+namespace demoan.Models
+{
+    public class Student
+    {
+        //cac phuong thuc thuoc tinh
+        public int StudentID { get; set; }
+        {
+            
+        }
+        public int Sum(int x, int y)
+    {
+        return x + y;
+    }
+        public int Employee(int manv, string tennv, string diachi)
+    {
+
+    }
+    public int Person(float cao, float nang,int tuoi)
+    {
+        return 1;
+    }
+        
+}
